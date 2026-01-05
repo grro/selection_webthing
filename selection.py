16e2dd24dc19e6009bf1e68643f9226307419d95
grro <gregror.roth@1und1.de>
2026-01-05 10:51:56 +0100

    silent selection support added

--- selection.py
+++ selection.py
@@ -1,39 +1,11 @@
-from time import sleep
 import logging
 import yaml
 from typing import List
-from threading import Thread
 from  pathlib import Path
 from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-
-
-class BackToDefaultTimer:
-
-    def __init__(self, selection, back_to_default_sec: int):
-        self.__selection = selection
-        self.__back_to_default_sec = back_to_default_sec
-
-    def start(self):
-        Thread(target=self.__run, daemon=True).start()
-
-    def __run(self):
-        while True:
-            try:
-                now = datetime.now()
-                elapsed = (now - self.__selection.selection_time).total_seconds()
-                if elapsed >= self.__back_to_default_sec:
-                    default_name = self.__selection.selection_names[0]
-                    if self.__selection.selected_name != default_name:
-                        logging.info(f"back to default selection '{default_name}' after {self.__back_to_default_sec} seconds")
-                        self.__selection.select(default_name)
-                sleep_time = max(1, int(self.__back_to_default_sec - elapsed))
-                sleep(sleep_time)
-            except Exception:
-                pass
-            sleep(3)
 
 
 class Selection(FileSystemEventHandler):
@@ -51,9 +23,6 @@ class Selection(FileSystemEventHandler):
         observer = Observer()
         observer.schedule(self, Path(file).parent, recursive=False)
         observer.start()
-        logging.info("back to default activated: " + str(back_to_default_sec) + " seconds (" + self.__options.get(self.selection_names[0]) + ")" if back_to_default_sec is not None else "back to default deactivated")
-        if back_to_default_sec < 60*60*24*365:
-            BackToDefaultTimer(self, back_to_default_sec).start()
 
     @property
     def selection_names(self) -> List:
